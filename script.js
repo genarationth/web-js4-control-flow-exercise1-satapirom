75580f5929e87c2d7b145af39cb036286142b38a
Satapirom Khawatthu. <Satapirom.12@gmail.com>
2024-05-13 15:26:01 +0700

    control flow exercise

---
+++ script.js
@@ -0,0 +1,29 @@
+let dayNumber = 7;
+
+if (dayNumber === 0){
+    console.log("Sunday");
+}
+else if(dayNumber === 7){
+    console.log ("Saturday");
+}
+else if(dayNumber === 6){
+    console.log ("Friday");
+}
+else if(dayNumber === 5){
+    console.log ("Thursday");
+}
+else if(dayNumber === 4){
+    console.log ("Wednesday");
+}
+else if(dayNumber === 3){
+    console.log ("Wednesday");
+}
+else if(dayNumber === 2){
+    console.log ("Tuesday");
+}
+else if(dayNumber === 1){
+    console.log ("Monday");
+}
+else{
+    console.log ("corresponding day of the week");
+}
